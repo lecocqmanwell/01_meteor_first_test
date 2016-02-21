7da012900600be9ea6d6277b18b2ae40fc663687
Manwell <lec.manoel@gmail.com>
2016-02-21 17:27:11 +0100

    login done

--- first_test.js
+++ first_test.js
@@ -61,7 +61,9 @@ if (Meteor.isClient) {
       // Insert a task into the collection
       Tasks.insert({
         text: text,
-        createdAt: new Date() // current time
+        createdAt: new Date(), // current time
+        owner: Meteor.userId(),  // _id of logged in user
+        username: Meteor.user().username  // username of logged in user
       });
 
       // Clear form
@@ -84,7 +86,11 @@ if (Meteor.isClient) {
     }
   });
 
+  Accounts.ui.config({
 
+    passwordSignupFields: "USERNAME_ONLY"
+
+  });
 
 
 }

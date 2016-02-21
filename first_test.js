fb71634723270f1674333cb5bc50440c034e2391
Manwell <lec.manoel@gmail.com>
2016-02-21 10:40:53 +0100

    app deployed

--- first_test.js
+++ first_test.js
@@ -1,22 +1,142 @@
-
+/* starter js code */
 
 if (Meteor.isClient) {
 
-  // This code only runs on the client
+   // This code only runs on the client
 
   Template.body.helpers({
 
-    tasks: [
+    tasks: function () {
 
-      { text: "This is task 1" },
+      // Show newest tasks at the top
 
-      { text: "This is task 2" },
+      return Tasks.find({}, {sort: {createdAt: -1}});
 
-      { text: "This is task 3" }
-
-    ]
+    }
 
   });
 
 }
+
+
+/*----- define a Task Collection,  -----*/
+
+
+
+Tasks = new Mongo.Collection("tasks");
+
+if (Meteor.isClient) {
+  // This code only runs on the client
+  Template.body.helpers({
+    tasks: function () {
+      return Tasks.find({});
+    }
+  });
+
+
+
+
+/* submit button ------ */
+
+  Template.body.events({
+    "submit .new-task": function (event) {
+     // Prevent default browser form submit
+      event.preventDefault();
+
+      // Get value from form element
+      var text = event.target.text.value;
+
+      // Insert a task into the collection
+      Tasks.insert({
+        text: text,
+        createdAt: new Date() // current time
+      });
+
+      // Clear form
+      event.target.text.value = "";
+    }
+  });
+
+ Template.task.events({
+    "click .toggle-checked": function () {
+      // Set the checked property to the opposite of its current value
+      Tasks.update(this._id, {
+        $set: {checked: ! this.checked}
+      });
+    },
+    "click .delete": function () {
+      Tasks.remove(this._id);
+    }
+  });
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
